c6034c2b5d67656326aed37108c206ca746efb2b
David-barce <71525685+David-barce@users.noreply.github.com>
2021-09-22 15:11:40 -0500

    Agregar archivos de proyecto.

---
+++ App2/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace App2.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+       {
+        private string _nombre;
+
+        public MainPageViewModel()
+        {
+            _nombre = "Holi";
+        }
+
+        public string Nombre 
+        {
+            
+            get => _nombre;
+            set
+            {
+                if (String.Equals(_nombre, value)) return;
+                var data = _nombre.CompareTo(value);
+
+                _nombre = value;
+                RaisePropertyChanged(nameof(Nombre));
+                RaisePropertyChanged(nameof(SinUpdate));
+
+            }
+        }
+
+       
+        public string SinUpdate
+        {
+            get => _nombre;
+        }
+    }
+}
---
+++ App2/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using App2.ViewModels;
+
+namespace App2.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string nombrePropiedad)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nombrePropiedad));
+        }
+    }
+}
